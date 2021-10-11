7d111c9f4d5c305a3e277924fc7d77dd626ddbb9
Okami2612 <51777169+Okami2612@users.noreply.github.com>
2021-10-11 16:34:06 +0700

    api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import { Route, Router, Switch } from "react-router-dom";
 import Cart from "./pages/Cart";
 import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductsList from "./pages/ProductsList";
 import Register from "./pages/Register";
+import Pay from "./pages/Pay";
+import Success from "./pages/Success";
 
 function App() {
   return (
     <Homepage/>
+    // <Router>
+    //   <Switch>
+    //     <Route exact path="/">
+    //       <Homepage />
+    //     </Route>
+    //     <Route path="/pay">
+    //       <Pay />
+    //     </Route>
+    //     <Route path="/success">
+    //       <Success />
+    //     </Route>
+    //   </Switch>
+    // </Router>
   );
 }
 
